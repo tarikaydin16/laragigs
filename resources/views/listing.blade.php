c4eec469e760711e7c06e54ca96d9ac4566a7045
tarikaydin16 <aydih.hosting@gmail.com>
2024-01-08 17:11:39 +0300

    added sections

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,3 +1,6 @@
-{{-- <h1>{{$heading}} </h1> --}}
-<h2> {{ $listing['title'] }}</h2>
-<p>{{ $listing['description'] }}</p>
+@extends('layout')
+@section('content')
+    {{-- <h1>{{$heading}} </h1> --}}
+    <h2> {{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+@endsection
