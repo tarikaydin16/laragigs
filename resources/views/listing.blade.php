f1bc7d832fca04d7b86be253058bd818ba41e1f1
tarikaydin16 <aydih.hosting@gmail.com>
2024-01-08 19:03:01 +0300

    added components

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -3,7 +3,7 @@
     <a href="index.html" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
-        <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+        <x-card>
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-48 mr-6 mb-6" src="{{ asset('images/no-image.png') }}" alt="" />
 
@@ -46,6 +46,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </x-card>
     </div>
 @endsection
