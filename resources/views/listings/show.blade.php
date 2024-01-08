1b851cb44223bb85f9e26c6873c3b1acebecb149
tarikaydin16 <aydih.hosting@gmail.com>
2024-01-08 20:41:40 +0300

    layout components

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,5 +1,4 @@
-@extends('layout')
-@section('content')
+<x-layout>
     <a href="index.html" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
@@ -35,4 +34,4 @@
             </div>
         </x-card>
     </div>
-@endsection
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -14,14 +15,5 @@ use App\Models\Listing;
 |
 */
  
-Route::get('/', function () {
-    return view('listings',[
-        'heading'=> 'Latest Listings',
-        'listings'=> Listing::all()
-    ]);
-});
-Route::get('listings/{listing}', function (Listing $listing) {
-    return view('listing',[
-        'listing'=> $listing
-    ]);
-});
+Route::get('/', [ListingController::class,'index']);
+Route::get('listings/{listing}', [ListingController::class,'show']);
